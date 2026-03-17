33eb7e3c4a4ddae6a074b13c56f1eb9525ecf34d
kthss01 <kthss01@naver.com>
2026-03-17 11:19:32 +0900

    refactor(ex01)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
-import HelloWorld from "./components/HelloWorld";
+import HelloWorld from "./components/ex01/HelloWorld";
 
 function App() {
 	return (
--- src/components/ex01/HelloWorld.tsx
+++ src/components/ex01/HelloWorld.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 interface HelloWorldProps {
 	name: string;
 	message: string;
 }
 
-const HelloWorld = ({ name, message }: HelloWorldProps) => {
+const HelloWorld = ({ name, message }: HelloWorldProps): React.JSX.Element => {
 	return (
 		<div>
 			안녕하세요, {name}님!
